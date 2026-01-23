63141ce4a55706036c54dbdf29db35ddac0585b6
Brendan Dileo <dileob23@gmail.com>
2026-01-22 20:40:42 -0500

    Update Home Activity class

--- app/src/main/java/com/brendanddev/systeminfo/HomeActivity.java
+++ app/src/main/java/com/brendanddev/systeminfo/HomeActivity.java
@@ -1,13 +1,32 @@
 package com.brendanddev.systeminfo;
 
+import android.content.Intent;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+
+public class HomeActivity extends BaseActivity {
+
+    private final static String TAG = "==== HomeActivity ====";
+    private final static String ACTIVITY = "Home";
+
+    @Override
+    protected String getLogTag() { return TAG; }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        TextView activityName = (TextView) findViewById(R.id.activityName);
+        activityName.setText(ACTIVITY);
+
+        TextView welcomeText = (TextView) findViewById(R.id.welcomeText);
+
+        Intent intent = getIntent();
+        String incomingText = intent.getStringExtra("username");
+
+        String welcomeMessage = String.format("Welcome %s!", incomingText);
+        welcomeText.setText(welcomeMessage);
     }
 }
