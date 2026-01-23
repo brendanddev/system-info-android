61f42e0aa5dbf4a68becb0d05f8ee86ccfb8f0cc
Brendan Dileo <dileob23@gmail.com>
2026-01-22 20:20:50 -0500

    Updates

--- app/src/main/java/com/brendanddev/systeminfo/BatteryInfo.java
+++ app/src/main/java/com/brendanddev/systeminfo/BatteryInfo.java
@@ -3,6 +3,9 @@ package com.brendanddev.systeminfo;
 public class BatteryInfo {
 
     private int batteryPercentage;
+    private String batteryHealth;
+    private double batteryTemp;
+    private boolean batteryExists;
 
 
 
--- app/src/main/java/com/brendanddev/systeminfo/LoginActivity.java
+++ app/src/main/java/com/brendanddev/systeminfo/LoginActivity.java
@@ -1,25 +1,44 @@
 package com.brendanddev.systeminfo;
 
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity implements View.OnClickListener {
 
-    private final static String TAG = "==== MainActivity ====";
+    private final static String TAG = "==== LoginActivity ====";
+    private final static String ACTIVITY = "Login";
+
 
     @Override
     protected String getLogTag() { return TAG; }
 
+    /**
+     *
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        TextView activityName = (TextView) findViewById(R.id.activityName);
+        activityName.setText(ACTIVITY);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+//        Intent switchActivity = new Intent(LoginActivity.this, )
 
     }
 
 
 
 }
+
+//Intent switchActivity = new Intent(MainActivity.this, SecondActivity.class);
+//startActivity(switchActivity);
