198118eef8fd43c5b6808d50abe6157952242ce3
Brendan Dileo <dileob23@gmail.com>
2026-01-22 19:23:17 -0500

    Implement method to get better percentage

--- app/src/main/java/com/brendanddev/systeminfo/MainActivity.java
+++ app/src/main/java/com/brendanddev/systeminfo/MainActivity.java
@@ -1,10 +1,17 @@
 package com.brendanddev.systeminfo;
 
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
 import android.os.Build;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private final static String TAG = "==== MainActivity ====";
+
+    @Override
+    protected String getLogTag() { return TAG; }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,25 @@ public class MainActivity extends AppCompatActivity {
         int sdk = Build.VERSION.SDK_INT;
         String board = Build.BOARD;
         String host = Build.HOST;
+    }
+
+
+    public int getBatteryPrecentage() {
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        Intent batteryIntent = getApplicationContext().registerReceiver(null, intentFilter);
+
+        try {
+            int level = batteryIntent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+            int scale = batteryIntent.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+
+            if (level != -1 && scale != -1) {
+                return (level * 100) / scale;
+            } else {
+                return -1;
+            }
+        } catch (Exception e) {
+            return -1;
+        }
     }
 
 
