c3f2aae515a5bf1e7dedb9a2d19e24fbdbb529c1
Brendan Dileo <dileob23@gmail.com>
2026-01-27 18:08:42 -0500

    Move model classes into model package

---
+++ app/src/main/java/com/brendanddev/systeminfo/model/BatteryInfo.java
@@ -0,0 +1,19 @@
+package com.brendanddev.systeminfo.model;
+
+public class BatteryInfo {
+
+    private int batteryPercentage;
+    private String batteryHealth;
+    private double batteryTemp;
+    private boolean batteryExists;
+
+
+
+
+
+
+
+
+
+
+}
