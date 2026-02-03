5b726a6607604cb9e974158c18f477dac3c62e25
Brendan Dileo <dileob23@gmail.com>
2026-02-02 20:32:29 -0500

    Create SettingsActivity

---
+++ app/src/main/java/com/brendanddev/systeminfo/activity/SettingsActivity.java
@@ -0,0 +1,19 @@
+package com.brendanddev.systeminfo.activity;
+
+import android.os.Bundle;
+import com.brendanddev.systeminfo.R;
+
+public class SettingsActivity extends BaseActivity {
+
+    private static final String TAG = "==== SettingsActivity ====";
+
+    @Override
+    protected String getLogTag() { return TAG; }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
