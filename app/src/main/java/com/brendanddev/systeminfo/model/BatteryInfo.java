922d1efd68fc6aae8be4176092f4f1bd7a06ba91
Brendan Dileo <dileob23@gmail.com>
2026-02-11 11:19:54 -0500

    Remove old layouts

--- app/src/main/java/com/brendanddev/systeminfo/model/BatteryInfo.java
+++
@@ -1,9 +0,0 @@
-package com.brendanddev.systeminfo.model;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
-
-public class BatteryInfo {
-}
