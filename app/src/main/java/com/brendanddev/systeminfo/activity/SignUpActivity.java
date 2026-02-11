4290d871eeb7aa55823511c39ffab89ef061c0e4
Brendan Dileo <dileob23@gmail.com>
2026-02-11 11:24:28 -0500

    Recreate SignUpActivity

---
+++ app/src/main/java/com/brendanddev/systeminfo/activity/SignUpActivity.java
@@ -0,0 +1,21 @@
+package com.brendanddev.systeminfo.activity;
+
+import android.os.Bundle;
+import com.brendanddev.systeminfo.R;
+
+public class SignUpActivity extends BaseActivity {
+    private static final String TAG = "==== SignUpActivity ====";
+
+
+    @Override
+    protected String getLogTag() { return TAG; }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+
+
+}
