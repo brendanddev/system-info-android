29a933a517b069d2c1c50c789260114ff38a61b4
Brendan Dileo <dileob23@gmail.com>
2026-02-21 12:33:50 -0500

    Add SignUpActivity layout

--- app/src/main/java/com/brendanddev/systeminfo/activity/SignUpActivity.java
+++ app/src/main/java/com/brendanddev/systeminfo/activity/SignUpActivity.java
@@ -1,8 +1,18 @@
 package com.brendanddev.systeminfo.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.brendanddev.systeminfo.R;
 
+/**
+ * Handles the Sign Up screen.
+ *
+ * @author Brendan Dileo
+ */
 public class SignUpActivity extends BaseActivity {
     private static final String TAG = "==== SignUpActivity ====";
 
@@ -10,10 +20,36 @@ public class SignUpActivity extends BaseActivity {
     @Override
     protected String getLogTag() { return TAG; }
 
+    /**
+     * Initializes the activity, sets up click listeners.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(this::onClickSignUp);
+
+        TextView loginRedirect = findViewById(R.id.loginRedirect);
+        loginRedirect.setOnClickListener(this::onClickLoginRedirect);
+    }
+
+    /**
+     * Handles the sign up button click.
+     * @param view The view that was clicked.
+     */
+    private void onClickSignUp(View view) {
+        Log.d(TAG, "Sign Up Button Clicked!");
+    }
+
+    /**
+     * Handles the login redirect text click.
+     * @param view The view that was clicked.
+     */
+    private void onClickLoginRedirect(View view) {
+        Log.d(TAG, "Login Redirect Text Clicked!");
+
     }
 
 
